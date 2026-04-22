c456c3c8f5fcb7bcb83a31fe5a62facabcc0be95
david musembi <dmusembi702@gmail.com>
2026-04-23 00:05:23 +0300

    Added CRUD operations

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, request,jsonify
+
+app = Flask(__name__)
+
+
+# CRUD  students
+class Student:
+    def __init__(self, id, name,course=None):
+        self.id=id
+        self.name=name
+        self.course=course
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "course": self.course
+        }    
+# In-memeory data
+
+students = [
+    Student(1, "David Musembi", "Software Development"),
+    Student(2, "Limotiz Bill", "Cyber Security")
+]
+
+def find_student(student_id):
+    for student in students:
+        if student.id == student_id:
+            return student
+    return None
+
+# Create
+@app.route('/student', methods=["POST"])
+def create_student():
+    data = request.json
+
+    new_student = Student(data["id"], data["name"], data["course"])
+    students.append(new_student)
+    return jsonify(new_student.to_dict()), 201
+
+# GET
+@app.route('/student')
+def fetch_student():
+    data = request.json
+    return jsonify([student.to_dict() for student in students]),200
+
+@app.route("/students/<int:student_id>", methods=["GET"])
+def fetch_student(student_id):
+    student = find_student(student_id)
+
+    if not student:
+        return jsonify({"error": "Student not found"}), 404
+
+    return jsonify(student.to_dict()), 200
+
+@app.route("/students/<int:student_id>", methods=["PUT"])
+def update_student(student_id):
+    student = find_student(student_id)
+
+    if not student:
+        return jsonify({"error": "Student not found"}), 404
+
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    student.name = data.get("name", student.name)
+    student.course = data.get("course", student.course)
+
+    return jsonify(student.to_dict()), 200
+
+#Delete
+@app.route("/students/<int:student_id>", methods=["DELETE"])
+def delete_student(student_id):
+    student = find_student(student_id)
+
+    if not student:
+        return jsonify({"error": "Student not found"}), 404
+
+    students.remove(student)
+
+    return jsonify({"message": "Student deleted successfully"}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
